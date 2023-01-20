5e876bbc2c6f3edcb66e5f62aa6f26efd93e2ac4
Massaya J (Jacqueline) <Jacqueline.Massaya@gov.scot>
2023-01-20 14:43:17 +0000

    Jackie QA - added functions script, tidied A1

--- June - A1.R
+++ June - A1.R
@@ -11,10 +11,9 @@
 
 rm(list=ls())
 
-# Set working directory
+#SAF Data drop file path
 
-
-setwd("//s0177a/datashare/seerad/ags/census/branch1/NewStructure/Surveys/June/Main/June21/Data Drops/SAF")
+SAF_directory <- ("//s0177a/datashare/seerad/ags/census/branch1/NewStructure/Surveys/June/Main/June21/Data Drops/SAF")
 
 # Load packages
 
@@ -23,6 +22,8 @@ library(data.table)
 library(dplyr)
 library(stringr)
 
+#Load functions
+source("Functions/A1 Functions.R")
 
 #  Assign year and month to allow creation of dataset with correct year (at the very end of this script)
 # Depending on how data stored, year/month may need to be specified in readxl
@@ -38,9 +39,9 @@ mnth<-"SEP"
 
 # Import datasets separately (or do this directly into a list depending on what other files are in the folder).
 
-df_permanent<-read_excel("permanent_output1_SecondDrop.xlsx")  # called script1 in SAS
-df_scheme<-read_excel("scheme_output1_SecondDrop.xlsx")  # called script2 in SAS
-df_seasonal<-read_excel("seasonal_output1_SecondDrop.xlsx") # called script3 in SAS
+df_permanent<-read_excel(paste0(SAF_directory, "/permanent_output1_SecondDrop.xlsx"))  # called script1 in SAS
+df_scheme<-read_excel(paste0(SAF_directory,"/scheme_output1_SecondDrop.xlsx"))  # called script2 in SAS
+df_seasonal<-read_excel(paste0(SAF_directory,"/seasonal_output1_SecondDrop.xlsx")) # called script3 in SAS
 
 
 
@@ -54,15 +55,11 @@ list_perm_seas<-list(df_permanent, df_seasonal)
 
 # Create line variable for list to index by LPID for permanent and seasonal. Number gives instance of each LPID. 
 
-list_perm_seas<-lapply(list_perm_seas, function(x)
-  
-  {setDT(x)[,line:=seq(1,.N),by=LPID]})
+list_perm_seas<-lapply(list_perm_seas, LPID_index)
 
 # Create new df and line variable for scheme
 
-df_schemework<-setDT(df_scheme)[,line:=seq(1,.N),by=LPID]
-
-
+df_schemework<- LPID_index(df_scheme)
 
 
 # Rename and create variables ---------------------------------------------
@@ -70,59 +67,15 @@ df_schemework<-setDT(df_scheme)[,line:=seq(1,.N),by=LPID]
 # Rename and create new variables for permanent and seasonal. Land Use is also renamed here for ease, as are landusearea and bpsclaimedarea.
 
 
-list_perm_seas<-lapply(list_perm_seas, function(x)
-  
-{rename(x, 
-        brn = "Business Reference Number",
-        mlc = "Main Location Code",
-        slc = "Location Code",
-        fid = LPID,
-        field_area = "Land Parcel Area",
-        lfass_eligible = LFASS,
-        eligible_area = "BPS Eligible Area",
-        LLO = "Land Leased Out",
-        land_use = "Land Use",
-        land_use_area = "Land Use Area",
-        bps_claimed_area = "BPS Claimed Area"
-)})
+list_perm_seas<-lapply(list_perm_seas, rename_perm_seas_vars)
 
-
-
-list_perm_seas<-lapply(list_perm_seas, function(x)
-
-  {mutate(x,
-    slc = substr(slc,4,11),
-    mlc = substr(mlc,4,11),
-    parish = str_remove(substr(slc,1,3), "^0+"),
-    holding = str_remove(substr(slc,5,8),"^0+"),
-    land_use = str_replace_all(land_use, "_", "-"),
-    sfp_area = bps_claimed_area,
-    sfp_code = land_use,
-    other_area = (land_use_area-bps_claimed_area),
-    other_code = land_use
-  )})
-
+list_perm_seas<-lapply(list_perm_seas, new_perm_seas_vars)
 
 # rename and create new variables in scheme.
 
-df_schemework<-df_schemework %>%
-  rename(
-    brn = "Business Reference Number",
-    mlc = "Main Location Code",
-    slc = "Location Code",
-    fid = LPID,
-  )
+df_schemework<- rename_scheme_vars(df_schemework)
 
-
-df_schemework<-df_schemework%>%
-  mutate(
-    slc = substr(slc,4,11),
-    mlc = substr(mlc,4,11),
-    parish = str_remove(substr(slc,1,3), "^0+"),
-    holding = str_remove(substr(slc,5,8),"^0+"),
-    SCHEME_OPTION_CODE = str_replace_all(SCHEME_OPTION_CODE, "_", "-") # not sure this line is necessary - there are no underscores or hyphens?
-  )
-
+df_schemework<-new_scheme_vars(df_schemework)
 
 
 
@@ -131,66 +84,41 @@ df_schemework<-df_schemework%>%
 # Create dfs for missing observations in permanent and seasonal. 
 # Note: this is looking for NAs rather than full stops or blanks as in the SAS code. R gives blanks as NAs, unlike in SAS. 
 
-missingperm_seas<-lapply(list_perm_seas, function(x)
-  
-  {subset(x,
-          is.na(brn) == TRUE | is.na(mlc) == TRUE | is.na(line) == TRUE | is.na(slc) == TRUE | is.na(fid) ==TRUE
-          )})
+missingperm_seas<-lapply(list_perm_seas, missing_obs)
 
 
 # Missing field area in permanent and seasonal. Again, we are looking for NAs and 0s (rather than full stops and 0s as in SAS).
 
-missfieldareaperm_seas<-lapply(list_perm_seas, function(x)
-  
-{x[field_area==0 | is.na(field_area)]
-  })
+missfieldareaperm_seas<-lapply(list_perm_seas, missing_field_area)
 
 
 # Missing eligible area in permanent and seasonal.
 
-misseligibleareaperm_seas<-lapply(list_perm_seas, function(x)
-  
-{x[is.na(eligible_area)]
-})
-
-
-
-
+misseligibleareaperm_seas<-lapply(list_perm_seas, missing_eligible_area)
 
 # Create final dataframes -------------------------------------------------
 
 # final list of permanent and seasonal removing blank records. 
 
-list_perm_seas<-lapply(list_perm_seas, function(x)
-  
-{x[!is.na(brn) | ! is.na(mlc) | !is.na(slc)]
-})
-
+list_perm_seas<-lapply(list_perm_seas, cleaned_datasets)
 
 # final scheme df removing blank records. 
 
 
-df_schemework<-df_schemework[!is.na(brn) | ! is.na(mlc) | !is.na(slc)]
+df_schemework<-cleaned_datasets(df_schemework)
 
 
 #  Create final dfs for permanent, scheme and seasonal. 
 
-# Unlist permanent and seaonal dfs (creating df1 and df2). Ideally should do this combining the following lines in one step...
+names(list_perm_seas) <- c("perm", "seas")
 
 for (i in seq(list_perm_seas))
-   assign(paste0("df", i), list_perm_seas[[i]])
+  assign(paste("allsaf", yr, names(list_perm_seas)[[i]], mnth, sep = "_"), list_perm_seas[[i]])
 
-
-# rename df with correct year
-
-assign(paste("allsaf", yr, "perm", mnth, sep = '_'), df1) 
-assign(paste("allsaf", yr, "seas", mnth, sep = '_'), df2) 
 assign(paste("allsaf", yr, "scheme", mnth, sep = '_'), df_schemework) 
 
 
 # This is where the A1 section of the SAS code ends.
-
-
 
 
 # Write to csv ------------------------------------------------------------
@@ -199,12 +127,12 @@ assign(paste("allsaf", yr, "scheme", mnth, sep = '_'), df_schemework)
 # Write dfs to csv for use in further scripts
 # Note: year and month are specified in the object name already...
 
-setwd("C:/Users/u455049/Documents/R/repos/Trial")
+output_path <- "C:/Users/u455049/Documents/R/repos/Trial"
 
 
-write.csv(allsaf_21_perm_SEP, "allsaf_21_perm_SEP.csv")
-write.csv(allsaf_21_scheme_SEP, "allsaf_21_scheme_SEP.csv")
-write.csv(allsaf_21_seas_SEP, "allsaf_21_seas_SEP.csv")
+write.csv(allsaf_21_perm_SEP, paste0(output_path, "/allsaf_21_perm_SEP.csv"))
+write.csv(allsaf_21_scheme_SEP, paste0(output_path, "/allsaf_21_scheme_SEP.csv"))
+write.csv(allsaf_21_seas_SEP, paste0(output_path, "/allsaf_21_seas_SEP.csv"))
 
 
 
