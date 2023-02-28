0a8b5ea5518fb697cec7e32ef2d7b1f16bf3e927
lucynevard <lucy.nevard@gov.scot>
2023-02-28 14:23:07 +0000

    Improved formatting

--- June - B6.R
+++ June - B6.R
@@ -3,17 +3,17 @@
 # Code corrections may need updating yearly depending on SAF data.
 # This script is based on the code in B4 and B6 of the June Project (\\s0177a\datashare\seerad\ags\census\branch1\NewStructure\Surveys\June\Main\JUNE CENSUS PROJECT - 2021 Provisional Scott)
 
-# Created by Lucy Nevard 27.01.23 
-# Modified by Lucy Nevard 27.02.23
+# Created by Lucy Nevard 27.01.23
+# Modified by Lucy Nevard 28.02.23
 
 
 # Before import -----------------------------------------------------------
 
 
 
-# Clear environment prior 
+# Clear environment prior
 
-rm(list=ls())
+rm(list = ls())
 
 
 # Datashare file path for import and export
@@ -43,36 +43,38 @@ source("Functions/Functions.R")
 
 # In the code from 2021, the data used last in B6 was AUG - I use SEP here for consistency with rest of previous code.
 
-allsaf_perm<-loadRData(paste0(Code_directory, "/allsaf_perm_A.rda"))
-allsaf_seas<-loadRData(paste0(Code_directory, "/allsaf_seas_A.rda"))
+allsaf_perm <- loadRData(paste0(Code_directory, "/allsaf_perm_A.rda"))
+allsaf_seas <- loadRData(paste0(Code_directory, "/allsaf_seas_A.rda"))
 
 
-list_perm_seas<-list(allsaf_perm,allsaf_seas)
+list_perm_seas <- list(allsaf_perm, allsaf_seas)
 
 
 
 #  Apply corrections to both datasets.  -----------------------------------
-# Correcting area variables and slc when blank. Code corrections for sfp_code and other_code. 
+# Correcting area variables and slc when blank. Code corrections for sfp_code and other_code.
 
-# For SAF codes - this might change year to year. But these below might still be used. 
-# Manual corrections will also be here and change from year to year. 
-# Make these manual changes part of a function in the future. Then codes can be updated in function script. 
+# For SAF codes - this might change year to year. But these below might still be used.
+# Manual corrections will also be here and change from year to year.
+# Make these manual changes part of a function in the future. Then codes can be updated in function script.
 
 
-  
-list_perm_seas<-lapply(list_perm_seas, function(x)
-  {mutate (x,
-          other_area=
-            ifelse(land_use_area<bps_claimed_area,"0",other_area),
-          sfp_area=
-            ifelse(land_use_area<bps_claimed_area,bps_claimed_area,sfp_area),
-          land_use_area=
-            ifelse(land_use_area<bps_claimed_area,bps_claimed_area,land_use_area),
-          slc= ifelse(slc=="", mlc, slc),
-          sfp_code = 
-            recode(as.factor(sfp_code),"TREE" = 'TREES',"UNSP" = 'EXCL'),
-          sfp_code = 
-            recode(as.factor(other_code),"TREE" = 'TREES',"UNSP" = 'EXCL'))})
+
+list_perm_seas <- lapply(list_perm_seas, function(x) {
+  mutate(x,
+    other_area =
+      ifelse(land_use_area < bps_claimed_area, "0", other_area),
+    sfp_area =
+      ifelse(land_use_area < bps_claimed_area, bps_claimed_area, sfp_area),
+    land_use_area =
+      ifelse(land_use_area < bps_claimed_area, bps_claimed_area, land_use_area),
+    slc = ifelse(slc == "", mlc, slc),
+    sfp_code =
+      recode(as.factor(sfp_code), "TREE" = "TREES", "UNSP" = "EXCL"),
+    sfp_code =
+      recode(as.factor(other_code), "TREE" = "TREES", "UNSP" = "EXCL")
+  )
+})
 
 
 
@@ -86,44 +88,45 @@ list_perm_seas<-lapply(list_perm_seas, function(x)
 # Tidy this up in future.
 
 
-for (i in seq(list_perm_seas))
+for (i in seq(list_perm_seas)) {
   assign(paste0("df", i), list_perm_seas[[i]])
+}
 
-allsaf_perm<-df1
-allsaf_seas<-df2
+allsaf_perm <- df1
+allsaf_seas <- df2
 
-rm(df1,df2)
+rm(df1, df2)
 
 
-allsaf_perm$landtype<-"PERM"
-allsaf_seas$landtype<-"SEAS"
+allsaf_perm$landtype <- "PERM"
+allsaf_seas$landtype <- "SEAS"
 
 
 # Remove rows of all NAS.
 
 
-allsaf_perm<-allsaf_perm[rowSums(is.na(allsaf_perm)) != ncol(allsaf_perm), ]
+allsaf_perm <- allsaf_perm[rowSums(is.na(allsaf_perm)) != ncol(allsaf_perm), ]
 
-allsaf_seas<-allsaf_seas[rowSums(is.na(allsaf_seas)) != ncol(allsaf_seas), ]
+allsaf_seas <- allsaf_seas[rowSums(is.na(allsaf_seas)) != ncol(allsaf_seas), ]
 
 
 
 # Permanent - split up SAF datalines into claimtype other or sfp --------
 
-# Reformat df with claimtype as OTHER or SFP and "line" variable increasing by 0.01 if OTHER. 
-# LLO flags also assigned 
+# Reformat df with claimtype as OTHER or SFP and "line" variable increasing by 0.01 if OTHER.
+# LLO flags also assigned
 
 # Other claimtype data
 
-allsaf_permother<-filter(allsaf_perm, allsaf_perm$other_area>0)
+allsaf_permother <- filter(allsaf_perm, allsaf_perm$other_area > 0)
 
-allsaf_permother$claimtype<-"OTHER"  
+allsaf_permother$claimtype <- "OTHER"
 
 # Tidy these bits up and make functions
 
-allsaf_permother$prefix<-substr(allsaf_permother$other_code,1,4)
+allsaf_permother$prefix <- substr(allsaf_permother$other_code, 1, 4)
 
-allsaf_permother<-allsaf_permother %>%
+allsaf_permother <- allsaf_permother %>%
   mutate(
     code = other_code,
     crops = other_code,
@@ -131,71 +134,73 @@ allsaf_permother<-allsaf_permother %>%
   )
 
 
-allsaf_permother$other_code<-as.character(allsaf_permother$other_code)
+allsaf_permother$other_code <- as.character(allsaf_permother$other_code)
 
 
 allsaf_permother <- allsaf_permother %>%
   mutate(
-    LLO=
-      ifelse(prefix=="LLO-","Y",ifelse(other_code=="LLO","Y","N")),
-    other_code=
-      ifelse(prefix=="LLO-", substring(other_code, 5),other_code),
-    other_code=
-      ifelse(other_code=="", "OTH-LAND",other_code))
+    LLO =
+      ifelse(prefix == "LLO-", "Y", ifelse(other_code == "LLO", "Y", "N")),
+    other_code =
+      ifelse(prefix == "LLO-", substring(other_code, 5), other_code),
+    other_code =
+      ifelse(other_code == "", "OTH-LAND", other_code)
+  )
 
 
 
 # SFP claimtype data
 
-allsaf_permsfp<-filter(allsaf_perm, allsaf_perm$sfp_area>0)
+allsaf_permsfp <- filter(allsaf_perm, allsaf_perm$sfp_area > 0)
 
-allsaf_permsfp$sfp_code<-as.character(allsaf_permsfp$sfp_code)
+allsaf_permsfp$sfp_code <- as.character(allsaf_permsfp$sfp_code)
 
-allsaf_permsfp<-allsaf_permsfp %>%
+allsaf_permsfp <- allsaf_permsfp %>%
   mutate(
     claimtype = "SFPS",
-    prefix = substr(sfp_code,1,4),
+    prefix = substr(sfp_code, 1, 4),
     code = sfp_code,
     crops = sfp_code,
     area = sfp_area
   )
 
 
-allsaf_permsfp<- allsaf_permsfp %>%
+allsaf_permsfp <- allsaf_permsfp %>%
   mutate(
-    LLO=
-      ifelse(prefix=="LLO-","1",ifelse(sfp_code=="LLO","Y","N")),
-    sfp_code=
-      ifelse(prefix=="LLO-", substring(sfp_code, 5),sfp_code),
-    sfp_code=
-      ifelse(sfp_code=="", "OTH-LAND",sfp_code),
-    line= 
-      ifelse(other_area>0, line+0.01, line))
+    LLO =
+      ifelse(prefix == "LLO-", "1", ifelse(sfp_code == "LLO", "Y", "N")),
+    sfp_code =
+      ifelse(prefix == "LLO-", substring(sfp_code, 5), sfp_code),
+    sfp_code =
+      ifelse(sfp_code == "", "OTH-LAND", sfp_code),
+    line =
+      ifelse(other_area > 0, line + 0.01, line)
+  )
 
 
 
 # Merge Other and SFP
 
 
-allsaf_perm <- rbind(allsaf_permother,allsaf_permsfp)
+allsaf_perm <- rbind(allsaf_permother, allsaf_permsfp)
 
 
 # Remove unneccesary variables
 
 
-allsaf_perm<-allsaf_perm %>%
-  select(-c(other_area,other_code,prefix))
+allsaf_perm <- allsaf_perm %>%
+  select(-c(other_area, other_code, prefix))
 
-# Create correct parish and holding 
+# Create correct parish and holding
 
-allsaf_perm<-allsaf_perm %>%
+allsaf_perm <- allsaf_perm %>%
   mutate(
-    parish = str_remove(substr(slc,1,3), "^0+"),
-    holding = str_remove(substr(slc,5,8),"^0+"),
+    parish = str_remove(substr(slc, 1, 3), "^0+"),
+    holding = str_remove(substr(slc, 5, 8), "^0+"),
   )
 
 
-# The SAS code here has tables to see what errors are left in the permanent df - not clear what wer'e looking for at this point and there aren't any fixes in the code, so I've left this out for now. 
+# The SAS code here has tables to see what errors are left in the permanent df - not clear what wer'e looking for at this point and there aren't any fixes in the code, so I've left this out for now.
 
 
 
@@ -205,27 +210,27 @@ allsaf_perm<-allsaf_perm %>%
 
 
 
-allsaf_seas<-filter(allsaf_seas, allsaf_seas$sfp_area>0)
+allsaf_seas <- filter(allsaf_seas, allsaf_seas$sfp_area > 0)
 
 # SFP claimtype data
 
-allsaf_seas<-allsaf_seas %>%
+allsaf_seas <- allsaf_seas %>%
   mutate(
     claimtype = "SFPS",
-    LLO="N",
+    LLO = "N",
     code = sfp_code,
     crops = sfp_code,
     area = sfp_area,
-    slc= ifelse(slc=="", mlc, slc)
+    slc = ifelse(slc == "", mlc, slc)
   )
 
 
 # Create correct parish and holding
 
-allsaf_seas<-allsaf_seas%>%
+allsaf_seas <- allsaf_seas %>%
   mutate(
-    parish = str_remove(substr(mlc,1,3), "^0+"),
-    holding = str_remove(substr(mlc,5,8),"^0+"),
+    parish = str_remove(substr(mlc, 1, 3), "^0+"),
+    holding = str_remove(substr(mlc, 5, 8), "^0+"),
   )
 
 
@@ -233,20 +238,20 @@ allsaf_seas<-allsaf_seas%>%
 
 # Save to datashare
 
-save(allsaf_perm,file=paste0(Code_directory, "/allsaf_perm_B6.rda"))
+save(allsaf_perm, file = paste0(Code_directory, "/allsaf_perm_B6.rda"))
 
-save(allsaf_seas,file=paste0(Code_directory, "/allsaf_seas_B6.rda"))
+save(allsaf_seas, file = paste0(Code_directory, "/allsaf_seas_B6.rda"))
 
 
 
 # Save to ADM
-# 
-# 
-# write_dataframe_to_db(server=server, 
-#                       database=database, 
-#                       schema=schema, 
-#                       table_name="allsaf_perm_B6", 
-#                       dataframe=allsaf_perm, 
+#
+#
+# write_dataframe_to_db(server=server,
+#                       database=database,
+#                       schema=schema,
+#                       table_name="allsaf_perm_B6",
+#                       dataframe=allsaf_perm,
 #                       append_to_existing = FALSE,
-#                       batch_size=1000, 
+#                       batch_size=1000,
 #                       versioned_table=FALSE)
