deb50fd192f035ce195588a45acdc3819733910c
lucynevard <lucy.nevard@gov.scot>
2023-03-16 16:15:25 +0000

    Tidied up a lot - made functions (in Functions script).

--- June - B6.R
+++ June - B6.R
@@ -7,6 +7,7 @@
 # Modified by Lucy Nevard 28.02.23
 
 
+
 # Before import -----------------------------------------------------------
 
 
@@ -14,6 +15,16 @@
 # Clear environment prior
 
 rm(list = ls())
+
+# Load packages
+
+library(tidyverse)
+library(dplyr)
+
+
+# Load functions
+
+source("Functions/Functions.R")
 
 
 # Datashare file path for import and export
@@ -26,29 +37,13 @@ server <- "s0196a\\ADM"
 database <- "RuralAndEnvironmentalScienceFarmingStatistics"
 schema <- "juneagriculturalsurvey2023alpha"
 
-# Load packages
-
-library(tidyverse)
-library(dplyr)
-
-# Load functions
-
-source("Functions/Functions.R")
-
-
-
 
 # Import SAF data -------------------------------------------------------------
 
 
 # In the code from 2021, the data used last in B6 was AUG - I use SEP here for consistency with rest of previous code.
 
-allsaf_perm <- loadRData(paste0(Code_directory, "/allsaf_perm_A.rda"))
-allsaf_seas <- loadRData(paste0(Code_directory, "/allsaf_seas_A.rda"))
-
-
-list_perm_seas <- list(allsaf_perm, allsaf_seas)
-
+list_perm_seas <- loadRData(paste0(Code_directory, "/saflist_permseas_A.rda"))
 
 
 #  Apply corrections to both datasets.  -----------------------------------
@@ -59,27 +54,7 @@ list_perm_seas <- list(allsaf_perm, allsaf_seas)
 # Make these manual changes part of a function in the future. Then codes can be updated in function script.
 
 
-
-list_perm_seas <- lapply(list_perm_seas, function(x) {
-  mutate(x,
-    other_area =
-      ifelse(land_use_area < bps_claimed_area, "0", other_area),
-    sfp_area =
-      ifelse(land_use_area < bps_claimed_area, bps_claimed_area, sfp_area),
-    land_use_area =
-      ifelse(land_use_area < bps_claimed_area, bps_claimed_area, land_use_area),
-    slc = ifelse(slc == "", mlc, slc),
-    sfp_code =
-      recode(as.factor(sfp_code), "TREE" = "TREES", "UNSP" = "EXCL"),
-    sfp_code =
-      recode(as.factor(other_code), "TREE" = "TREES", "UNSP" = "EXCL")
-  )
-})
-
-
-
-
-
+list_perm_seas <- lapply(list_perm_seas, change_codes)
 
 
 # Unlist permanent and seasonal.  ---------------------------------------
@@ -87,28 +62,25 @@ list_perm_seas <- lapply(list_perm_seas, function(x) {
 
 # Tidy this up in future.
 
+#  Create final dfs for permanent, scheme and seasonal.
+
+names(list_perm_seas) <- c("perm", "seas")
 
 for (i in seq(list_perm_seas)) {
-  assign(paste0("df", i), list_perm_seas[[i]])
+  assign(paste("saf", names(list_perm_seas)[[i]], sep = "_"), list_perm_seas[[i]])
 }
 
-allsaf_perm <- df1
-allsaf_seas <- df2
 
-rm(df1, df2)
-
-
-allsaf_perm$landtype <- "PERM"
-allsaf_seas$landtype <- "SEAS"
+saf_perm$landtype <- "PERM"
+saf_seas$landtype <- "SEAS"
 
 
-# Remove rows of all NAS.
+# Remove rows of all NAS. Couldn't figure out how to do this in a list.
 
 
-allsaf_perm <- allsaf_perm[rowSums(is.na(allsaf_perm)) != ncol(allsaf_perm), ]
+saf_perm <- saf_perm[rowSums(is.na(saf_perm)) != ncol(saf_perm), ]
 
-allsaf_seas <- allsaf_seas[rowSums(is.na(allsaf_seas)) != ncol(allsaf_seas), ]
-
+saf_seas <- saf_seas[rowSums(is.na(saf_seas)) != ncol(saf_seas), ]
 
 
 # Permanent - split up SAF datalines into claimtype other or sfp --------
@@ -118,129 +90,64 @@ allsaf_seas <- allsaf_seas[rowSums(is.na(allsaf_seas)) != ncol(allsaf_seas), ]
 
 # Other claimtype data
 
-allsaf_permother <- filter(allsaf_perm, allsaf_perm$other_area > 0)
+saf_permother <- filter(saf_perm, saf_perm$other_area > 0)
 
-allsaf_permother$claimtype <- "OTHER"
+# 
 
-# Tidy these bits up and make functions
-
-allsaf_permother$prefix <- substr(allsaf_permother$other_code, 1, 4)
-
-allsaf_permother <- allsaf_permother %>%
-  mutate(
-    code = other_code,
-    crops = other_code,
-    area = other_area
-  )
-
-
-allsaf_permother$other_code <- as.character(allsaf_permother$other_code)
-
-
-allsaf_permother <- allsaf_permother %>%
-  mutate(
-    LLO =
-      ifelse(prefix == "LLO-", "Y", ifelse(other_code == "LLO", "Y", "N")),
-    other_code =
-      ifelse(prefix == "LLO-", substring(other_code, 5), other_code),
-    other_code =
-      ifelse(other_code == "", "OTH-LAND", other_code)
-  )
-
+saf_permother <- newvarsother(saf_permother)
 
 
 # SFP claimtype data
 
-allsaf_permsfp <- filter(allsaf_perm, allsaf_perm$sfp_area > 0)
-
-allsaf_permsfp$sfp_code <- as.character(allsaf_permsfp$sfp_code)
-
-allsaf_permsfp <- allsaf_permsfp %>%
-  mutate(
-    claimtype = "SFPS",
-    prefix = substr(sfp_code, 1, 4),
-    code = sfp_code,
-    crops = sfp_code,
-    area = sfp_area
-  )
+saf_permsfp <- filter(saf_perm, saf_perm$sfp_area > 0)
 
 
-allsaf_permsfp <- allsaf_permsfp %>%
-  mutate(
-    LLO =
-      ifelse(prefix == "LLO-", "1", ifelse(sfp_code == "LLO", "Y", "N")),
-    sfp_code =
-      ifelse(prefix == "LLO-", substring(sfp_code, 5), sfp_code),
-    sfp_code =
-      ifelse(sfp_code == "", "OTH-LAND", sfp_code),
-    line =
-      ifelse(other_area > 0, line + 0.01, line)
-  )
-
+saf_permsfp <- newvarssfp(saf_permsfp)
 
 
 # Merge Other and SFP
 
 
-allsaf_perm <- rbind(allsaf_permother, allsaf_permsfp)
+saf_perm_final <- rbind(saf_permother, saf_permsfp)
 
 
-# Remove unneccesary variables
+# Remove unnecesary variables
 
 
-allsaf_perm <- allsaf_perm %>%
+saf_perm_final <- saf_perm_final %>%
   select(-c(other_area, other_code, prefix))
 
 # Create correct parish and holding
 
-allsaf_perm <- allsaf_perm %>%
-  mutate(
-    parish = str_remove(substr(slc, 1, 3), "^0+"),
-    holding = str_remove(substr(slc, 5, 8), "^0+"),
-  )
+saf_perm_final2 <- parishholdingslc(saf_perm_final)
 
 
-# The SAS code here has tables to see what errors are left in the permanent df - not clear what wer'e looking for at this point and there aren't any fixes in the code, so I've left this out for now.
 
-
+# The SAS code here has tables to see what errors are left in the permanent df - not clear what we're looking for at this point and there aren't any fixes in the code, so I've left this out for now.
 
 
 #  Seasonal - filter out "Other" holdings, keep only SFP  --------------------------------------------------------------
 # According to desk notes, the data from Other is too messy to be reliable. The seasonal data is therefore an underestimate.
 
 
-
-allsaf_seas <- filter(allsaf_seas, allsaf_seas$sfp_area > 0)
+saf_seas <- filter(saf_seas, saf_seas$sfp_area > 0)
 
 # SFP claimtype data
 
-allsaf_seas <- allsaf_seas %>%
-  mutate(
-    claimtype = "SFPS",
-    LLO = "N",
-    code = sfp_code,
-    crops = sfp_code,
-    area = sfp_area,
-    slc = ifelse(slc == "", mlc, slc)
-  )
+saf_seas<-newvarsseas(saf_seas)
 
 
 # Create correct parish and holding
 
-allsaf_seas <- allsaf_seas %>%
-  mutate(
-    parish = str_remove(substr(mlc, 1, 3), "^0+"),
-    holding = str_remove(substr(mlc, 5, 8), "^0+"),
-  )
-
+saf_seas<-parishholdingmlc(saf_seas)
 
 # Save separate permanent and seasonal datasets --------------------------
 
 # Save to datashare
 
-save(allsaf_perm, file = paste0(Code_directory, "/allsaf_perm_B6.rda"))
+save(saf_perm, file = paste0(Code_directory, "/saf_perm_B6.rda"))
 
-save(allsaf_seas, file = paste0(Code_directory, "/allsaf_seas_B6.rda"))
+save(saf_seas, file = paste0(Code_directory, "/saf_seas_B6.rda"))
 
 
 
