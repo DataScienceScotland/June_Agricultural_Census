fea70716e868ae68fcd2759b5e561d8f22f25349
Massaya J (Jacqueline) <Jacqueline.Massaya@gov.scot>
2023-04-26 12:34:39 +0100

    JAC-C validations- need to make small fixes for crofting and produce read out report.

---
+++ item_numbers.R
@@ -0,0 +1,428 @@
+##Individual item numbers JAC 2023
+
+#NB new item numbers for 2023 have 5 digits
+
+
+#Section 1---------------------------------------------------------------------
+#Area you own
+area_own <- "item11"
+
+#Area of land you rent in on a full tenancy at this location
+area_rent <- "item20026"
+area_rent_21 <- "item7"
+
+#Total area
+total_area <- "item12"
+
+
+#Crofts
+owned_croft <- "item2249"
+rented_croft <- "item2251"
+
+#Section 2---------------------------------------------------------------------
+#Land seasonally rented in from another person- same location as last year
+seas_rent_same_in <- "item2827"
+#Land seasonally rented in from another person- did not rent this location last year
+seas_rent_diff_in <- "item2828"
+#total area of seasonal lets rented in
+seas_total_area_in <- "item2"
+#Land seasonally rented out to another person- area of land recorded in section 1 that you let out seasonally to another person
+seas_area_rent_out <- "item1"
+
+#Section 3---------------------------------------------------------------------
+
+#Cereals and oilseeds - SAF only 
+winter_barley <- "item16"
+spring_barley <- "item18"
+winter_oats <- "item17"
+spring_oats <- "item20"
+winter_rape <- "item19"
+spring_rape <-  "item23"
+
+#Cereals and oilseed - census form
+all_barley <- "item27710"
+all_oats <- "item27715"
+wheat <- "item14"
+all_rape <- "item27720"
+
+#Potatoes 
+#Potatoes intended mainly for seed
+seed_potatoes <- "item24"
+#Potatoes intended mainly for ware
+ware_potatoes <- "item2320"
+
+
+#Stockfeed  SAF- only
+#Turnips and swede
+turn_swed_sf <- "item29"
+#maize
+maize <- "item2059"
+#other crops including cereal for silage
+other_sf <- "item34"
+#rape
+rape_sf <- "item31"
+#fodder beet
+fbeet_sf <- "item32"
+#Kale and cabbage
+kale_cab_sf <- "item30"
+
+#Stockfeed - census form
+#All stockfeed – turnips, swedes, kale, rape, maize, fodder beet, silage crops
+all_stockfeed <- "item27725"
+
+#Veg for human consumption - SAF only
+peas <- "item52"
+beans <- "item53"
+leeks <- "item55"
+#turnips and swedes
+turn_swed <- "item56"
+#cabbages and savoys
+cab_sav <- "item2323"
+#Brussels Sprouts
+brussel <- "item59"
+#Calabrese/sprouting broccoli
+calabrese <- "item60"
+#cauliflowerand broccoli-heading varieties
+cauli_broc <- "item61"
+carrots <- "item63"
+lettuce <- "item64"
+rhubarb <- "item65"
+#Other vegetables – area of individual crops which are too small to be sown separately
+other_veg <- "item66"
+#Total vegetables
+all_veg_saf <- "item68"
+
+
+#veg- census form
+#All vegetables – e.g. peas, beans, leeks, turnips, carrots, lettuce etc.
+all_veg_open <- "item27730"
+all_veg_open_21 <- "item35"
+
+#Fruit grown in open - SAF only
+strawberries <- "item70"
+raspberries <- "item71"
+blackcurrants <- "item72"
+blueberries <- "item2832"
+orchard_fruit <- "item36"
+mixed_soft_fruit <- "item75"
+all_fruit_saf <- "item76"
+soft_fruits <- "item37"
+
+#fruit - census form
+#All fruit – strawberries, raspberries, blackcurrant, blueberries, orchard etc.
+all_fruit <- "item27735"
+
+#flowers, bulbs and nursery stock grown in the open- SAF only
+
+#Bulbs/flowers grown for the production of dry bulbs and/or cut flowers in the open
+all_flow_bulb_SAF <- "item2324"
+#Bedding and pot plants grown in the open
+bed_pot_plant <- "item1709"
+#Fruit stocks – spawn beds, runner beds, stool beds and young plants intended for sale
+fruit_stocks <- "item80"
+#Roses and rose stocks
+roses_stocks <- "item81"
+shrubs <- "item1710"
+#Ornamental trees (not forest trees)
+orn_trees <- "item82"
+#Other nursery stock – herbaceous plants, alpines, etc. (not forest trees)
+other_nurs <- "item83"
+
+#flowers, bulbs and nursery stock grown in the open- census form
+#All flowers, bulbs and nursery stocks
+all_flow_bulb <- "item27740"
+all_flow_bulb_21 <- "item84"
+
+#glasshouses- SAF only
+#Open soil
+glass_tom_open <- "item2713"
+glass_other_fruit_open <- "item2714"
+glass_veg_open <- "item2715"
+glass_bed_pot_plant_open <- "item2716"
+glass_nurs_stock_open <- "item2717"
+glass_strawb_open <- "item2858"
+glass_rasp_open <- "item2859"
+glass_blue_open <- "item2860"
+glass_black_open <- "item2861"
+
+
+#solid floor
+glass_tom_solid <- "item2707"
+glass_other_fruit_solid <-"item2708"
+glass_veg_solid <-"item2709"
+glass_bed_pot_plant_solid <-"item2710"
+glass_nurs_stock_solid <- "item2711"
+glass_strawb_solid <- "item2863"
+glass_rasp_solid <- "item2864"
+glass_blue_solid <- "item2865"
+glass_black_solid <- "item2866"
+
+#Walk in plastic structure greenhouse (Total open soil area)
+tot_open_plastic <- "item85"
+#Glass clad structure greenhouses (Total solid floor area)
+tot_solid_glass <- "item86"
+protect_tom <- "item87"
+
+#glasshouses- census form
+#All crops/plants
+all_glass_open <- "item27750"
+all_glass_solid <- "item27755"
+#Unused area
+glass_unused_solid <- "item2867"
+glass_unused_open <- "item2862"
+#other crops
+other_crops <- "item41"
+other_crops_text <- "item185"
+
+#Fallow, Grass, Woodland, Other land- census form
+#Fallow land for 5 years or less
+fland_lte5 <- "item2469"
+#Fallow land for more than 5 years
+fland_gt5 <- "item2470"
+#Grass under 5 years old (seeded or re-seeded in the last 5 years)
+grass_lt5 <- "item2321"
+#Grass 5 years old and older (not seeded or re-seeded in the last 5 years)
+grass_gte5 <- "item2322"
+#Rough grazings – mountain, hill, moor, deer forest situated within the farming unit, enclosed or not. Do not include woods, roads, share in common grazings or land taken seasonally
+rough_graze <- "item47"
+#Woodland – total area (ha.) of woodland (other than orchards) on the location, including woodland used for commercial or amenity purposes and forest trees
+woodland <- "item48"
+#Other land – roads, yards, buildings (excluding glasshouses), ponds, derelict land, etc.
+other_land <- "item49"
+
+#Total Area of Crops and Grass-SAF
+total_crops_grass <- "item46"
+
+#Total LAND 
+#TOTAL LAND from Section 3 – should equal Total Area Section 1 box 3 (item12)
+total_land <-  "item50"
+
+#Section 4---------------------------------------------------------------------
+#Pigs
+#Sows in pig
+sows_pig <- "item146"
+#Gilts in pig
+gilts_pig <- "item147"
+#Other sows for breeding
+other_sows_pig <- "item148"
+#Barren sows for fattening
+barren_sows_pig <- "item149"
+#Gilts 50 kg and over, not yet in pig, but expected to be used for breeding
+gilts_50_pig <- "item150" 
+#Boars being used for service
+boars_pig <- "item151"
+#Fattening pigs (over 29 kg)
+fat_pig <- "item27760"
+#Weaners (29 kg or less)
+wean_pig <- "item27765"
+#Piglets/suckling pigs
+piglet <- "item27770"
+#total pigs-  items 146-156
+total_pig <- "item157"
+
+#----------items used up to 2021
+#Other pigs 110kg and over
+other_110_pig <- "item152"
+#Other pigs 80-110kg
+other_80_110_pig <- "item153"
+#Other pigs 50-80kg
+other_50_80_pig <- "item154"
+#Other pigs 20-50kg
+other_20_50_pig <- "item155"
+#Other pigs under 20kg
+other_lt20_pig <- "item156"
+
+#Section 5---------------------------------------------------------------------
+#Sheep
+#Ewes that were used for breeding in 2023 – number still on farm at 1 June 20xx
+ewes <- "item139"
+#Rams to be used/expected to be used for service 
+rams <- "item140"
+#Other sheep 1 year and over used for breeding
+breed_other_sheep <- "item141"
+#Other sheep 1 year and over NOT used for breeding
+no_breed_other_sheep <- "item143"
+#Lambs
+lambs <- "item144"
+#Total sheep (items 139-141, 143-144)
+total_sheep <- "item145"
+
+#Section 6---------------------------------------------------------------------
+#Poultry
+#Fowls laying eggs for eating: hens in the laying stock: hens in the first laying season
+first_hens <- "item158"
+#Fowls laying eggs for eating: hens in the laying stock: moulted hens
+moulted_hens <- "item159"
+#Fowls laying eggs for eating: pullets being reared for laying
+pullets <- "item161"
+#Fowls of all ages for breeding: females laying eggs: to hatch layer chicks
+layer_chicks <- "item160"
+#Fowls of all ages for breeding: females laying eggs: to hatch table chicks
+table_chicks <- "item162"
+#Fowls of all ages for breeding: cocks
+cocks <- "item163"
+#Broilers and other table fowls
+broilers <- "item164"
+turkeys <- "item1708"
+ducks <- "item2038"
+geese <- "item2039"
+other_poultry <- "item167"
+#total poultry(item158-164, item167, item1708, item2038-2039)
+total_poultry <- "item170"
+
+
+#Section 7 ---------------------------------------------------------------------
+#Miscellaneous Livestock
+#Deer of all ages and types (exclude wild or park deer which cannot be gathered, identified, recorded and handled)
+deer <- "item94"
+donkeys <- "item2868"
+alpacas <- "item2472"
+llamas <- "item2473"
+other_camelids <- "item2474"
+#Hives containing a colony of live honey bees, whether owned by you or not
+hives <- "item2826"
+#Other farm livestock not previously mentioned – do not include cattle/buffalo here
+other_livestock <- "item171"
+#Specify type of livestock entered in box 9 (item171) above (please PRINT here)	
+other_livestock_txt <- "item186"
+#All horses and ponies
+horse_pony <- "item27775"
+#All goats and kids
+goats_kids <- "item27780"
+
+#-----------------items used up to 2021
+#Horses Used in Agriculture
+agri_horses <- "item95"
+#All Other Horses and Ponies
+other_horse_pony <- "item96"
+#All Other Goats and kids
+other_goats_kids <- "item98"
+#Female Goats which have kidded this year or will kid this year
+female_goat_kids <- "item1712"
+#Other Female Goats which have been used for breeding in previous years
+other_female_goat <- "item1713"
+
+#Section 8 ---------------------------------------------------------------------
+#Legal Responsibility
+#Do not return your details on more than one location. If you have already returned your details on another 1 June 2023 census form, please tick this box and go on to Section 10
+other_legal_return <- "item2727"
+#Who has legal and financial responsibility for this location?
+legal_fin_resp <- "item2980"
+#Section 9 ---------------------------------------------------------------------
+#Occupier(s)
+#Occupier 1 details - legal responsibility is assumed
+occupier1_male <-"item2877"
+occupier1_female <- "item2878"
+#Year of birth – enter format ‘YYYY’
+occupier1_year <- "item27785"
+#Occupier 1 – proportion of time spent working on this location:
+#full-time
+occupier1_ft <- "item177"
+#Part-time: half-time or more
+occupier1_pt_gthalf <- "item178"
+#Part-time: less than half-time
+occupier1_pt_lthalf <- "item179"
+#no farm work on this location
+occupier1_no_work <- "item2566"
+
+#Occupier 2 details - Occupier 2 – can include formalised relationships (married couples and civil partnerships), cohabiting couples and close family relationships (father, mother, daughter, son and sibling)
+occupier2_male <-"item3056"
+occupier2_female <- "item3057"
+occupier2_legal_fin <- "item27805"
+#Year of birth – enter format ‘YYYY’
+occupier2_year <- "item27795"
+#Occupier 2 – proportion of time spent working on this location:
+#full-time
+occupier2_ft <- "item182"
+#Part-time: half-time or more
+occupier2_pt_gthalf <- "item183"
+#Part-time: less than half-time
+occupier2_pt_lthalf <- "item184"
+#no farm work on this location
+occupier2_no_work <- "item2567"
+
+#-----------------items used up to 2021 (spouse = occupier 2 from 2023, item numbers remain the same)
+#Do not return your details on more than one location form.If you have already returned your details on another 2018 census form, please tick this box and go on to Section 9
+other_legal_return_21 <- "item2726"
+# spouse_male <-"item3056"
+# spouse_female <- "item3057"
+# #spouse– proportion of time spent working on this location:
+# #full-time
+# spouse_ft <- "item182"
+# #Part-time: half-time or more
+# spouse_pt_gthalf <- "item183"
+# #Part-time: less than half-time
+# spouse_pt_lthalf <- "item184"
+# #no farm work on this location
+# spouse_no_work <- "item2567"
+
+#Section 10 ---------------------------------------------------------------------
+#Other labour on the location
+#Full-time regular staff employed on 1 June 2023: Males:
+#Business partners
+labour_ft_m_bp <- "item1714"
+labour_ft_m_hired <- "item1715"
+#members of occupier's family
+labour_ft_m_fam <- "item1716"
+
+#Full-time regular staff employed on 1 June 2023: Females:
+#Business partners
+labour_ft_f_bp <- "item1717"
+labour_ft_f_hired <- "item192"
+#members of occupier's family
+labour_ft_f_fam <- "item193"
+
+#Part-time regular staff employed on 1 June 2023: Males:
+#Business partners
+labour_pt_m_bp <- "item1718"
+labour_pt_m_hired <- "item194"
+#members of occupier's family
+labour_pt_m_fam <- "item195"
+
+#Part-time regular staff employed on 1 June 2023: Females:
+#Business partners
+labour_pt_f_bp <- "item1719"
+labour_pt_f_hired <- "item196"
+#members of occupier's family
+labour_pt_f_fam <- "item197"
+
+#Casual and seasonal staff employed on 1 June 2023
+#males
+labour_cas_m <- "item198"
+#females
+labour_cas_f <- "item199"
+
+#Total regular, casual and seasonal staff (Boxes 1 to 14 above -all items in section 10)
+total_labour <- "item200"
+
+#Section 11 --------------------------------------------------------------------- 
+#Non-regular, contract labour and migrant workers
+#Non-family labour employed on non-regular basis
+labour_non_fam_non_reg <- "item2712"
+#Agricultural contract labour (include workers who are employed by a third party)
+labour_agri_contract <- "item2066"
+#EU migrant workers (i.e. workers that are not UK nationals)- new for 2023
+labour_EU_migrant <- "item2536"
+#Non-EU migrant workers (i.e. workers that are not UK nationals) 
+#Note that the definition of 2511 has changed (before 2023 = Migrant workers (i.e. workers that are not UK nationals)). For comparability, pre-2023 item2511 = newitem2511+newitem2536 
+labour_non_EU_migrant <- "item2511"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
