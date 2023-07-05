bbcb6223eb276e1179fd174246513fd6054b79b3
Massaya J (Jacqueline) <Jacqueline.Massaya@gov.scot>
2023-07-05 11:05:26 +0100

    more module validations

--- C Validations/Module validations.R
+++ C Validations/Module validations.R
@@ -33,7 +33,16 @@ server <- "s0196a\\ADM"
 database <- "RuralAndEnvironmentalScienceFarmingStatistics"
 schema <- "juneagriculturalsurvey2023alpha"
 
+
+#Core item list
+
+#Section 1 - Area of location----------------------------------------------------------------------------------------------------------------------------
+#Total area
+total_area <- "item12"
+
 #Module item list----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 #Section 12 – Agricultural production methods---------------------------------------------------------------
 
 rough_grazing_woodland_only <- "item20705"
@@ -106,6 +115,7 @@ organic_fertilisers_spread	<- "item5103"
 
 
 #What percentage of the manure/slurry spread on your holding is spread using the following techniques? 
+broadcast_spreader_less_four_hours <- "item5108"
 broadcast_spreader_more_four_hours	<- "item5109"
 broadcast_spreader_not_ploughed	<- "item5110"
 band_spreader_hose	<- "item5111"
@@ -139,11 +149,24 @@ separate_applications	<- "item2663"
 
 section_13 <- c(ghouse_fallow_woodland_oland_only, fertiliser_mineral_area, fertiliser_organic_area, manure_use, solid_manure_export,
                 solid_manure_import, slurry_used, slurry_export, slurry_import, mineral_fertilisers_spread, organic_fertilisers_spread,
-                broadcast_spreader_more_four_hours, broadcast_spreader_not_ploughed, band_spreader_hose, band_spreader_shoe,
+                broadcast_spreader_less_four_hours, broadcast_spreader_more_four_hours, broadcast_spreader_not_ploughed, band_spreader_hose, band_spreader_shoe,
                 open_slot_shallow_injection, closed_slot_deep_injection, other_spread, storage_heaps, storage_compost,
                 storage_pits_below_animals, storage_deep_litter, storage_without_cover, storage_permeable_cover, storage_impermeable_cover,
                 storage_other_facilities, daily_spread, other_systems, manure_compost_months, manure_pits_months, manure_deep_litter_months,
                 slurry_months, manure_other_facilities_months, tested_nutrient_value, separate_applications)
+
+
+#Module error descriptions ------------------------------------------
+
+merr1_desc <- "merr1_Total percentage of the manure/slurry spread not equal to 100%"
+merr2_desc <- "merr2_Total percentage of the manure/slurry storage not equal to 100%"
+merr3_desc <- "merr3_Indicated nutrient management but no area of grassland or cropland given"
+merr4_desc <- "merr4_Sum of general crop rotation land and land not included in general crop rotation differs from total area (item12) by 3 ha or more"
+merr5_desc <- "merr5_Sum of area pH tested (grassland and cropland) is greater than total area (item12)"
+merr6_desc <- "merr6_Sum of area nutrient management plan (grassland and cropland) is greater than total area (item12)"
+merr7_desc <- "merr7_Sum of total farmed area fertilised with mineral and manure fertilisers greater than total area (item12)"
+
+
 # Import ------------------------------------------------------------------
 
 #combined dataset (SAF and JAC data)
@@ -214,15 +237,15 @@ module_section_counts <- data.frame(count_no_section_12, count_yes_section_12, c
 
 #percentage checks-------------------------------------------------------------------------------------------------
 
-JAC_module_23<- all_JAC_form %>% select(parish, holding, #submisType, 
-                                        land_data, saf_data, all_of(c(section_12, section_13)))
+JAC_module_23<- all_JAC_form %>% select(parish, holding, submisType, 
+                                        land_data, saf_data, all_of(c(section_12, section_13, total_area)))
 
 #check number of 0 responses across module
 no_response <- JAC_module_23 %>% ungroup %>% select(-parish, -holding, -land_data, -saf_data, ) %>% mutate(no_response = rowSums(.)) %>% filter(no_response == 0) %>% 
   nrow(.)
 
 #manure/slurry spread percentage
-JAC_module_23 <- JAC_module_23 %>%  mutate(manure_slurry_spread_total = sum(get(broadcast_spreader_more_four_hours), get(broadcast_spreader_not_ploughed),
+JAC_module_23 <- JAC_module_23 %>%  mutate(manure_slurry_spread_total = sum(get(broadcast_spreader_less_four_hours), get(broadcast_spreader_more_four_hours), get(broadcast_spreader_not_ploughed),
                                                                      get(band_spreader_hose), get(band_spreader_shoe), get(open_slot_shallow_injection), 
                                                                      get(closed_slot_deep_injection), get(other_spread)))
 
@@ -246,24 +269,72 @@ JAC_module_23 <- JAC_module_23 %>% mutate(merr2 = case_when(manure_slurry_stored
 
 #merr3 - indicated nutrient management but no area of grassland or cropland given
 
-JAC_module_23 <- JAC_module_23 %>% mutate(merr3 = case_when(nutrient_plan_update ==1 & (area_ph_crop == 0 |
-                                                                                          area_ph_grass ==0) ~ 1,
+JAC_module_23 <- JAC_module_23 %>% mutate(merr3 = case_when(nutrient_plan_update ==1 & (get(nutrient_plan_crop) == 0 |
+                                                                                          get(nutrient_plan_grass) ==0) ~ 1,
                                                             TRUE ~0))
+
+#merr4 - sum of general crop rotation land and land not included in general crop rotation differs from total area (item12) by 3 ha or more
+#check in 'module error dataframes' section to see if land has been erroneously portioned between general crop rotation, not in general crop rotation and arable mix with legume
+#should be general crop rotation/not in general crop rotation. 
+
+#create crop_rot_sum (general crop rot + not in general crop rot) 
+JAC_module_23 <- JAC_module_23 %>% mutate(crop_rot_sum = sum(get(crop_rotation), get(not_crop_rotation)))
+
+#merr4 = 1 when difference between crop_rot_sum and total area > 3 
+JAC_module_23 <- JAC_module_23 %>% mutate(merr4 = case_when(crop_rot_sum > 0 & abs(round(crop_rot_sum, digits = 2) - round(get(total_area), digits = 2)) >= 3 ~ 1,
+                                                            TRUE~0))
+#add column to give difference between crop_rot_sum and total area 
+JAC_module_23 <- JAC_module_23 %>% mutate(merr4_diff = case_when(merr4 == 1 ~ abs(crop_rot_sum - get(total_area)),
+                                                            TRUE~0))
+
+#merr5 - sum of area pH tested (grassland and cropland) is greater than total area (item12)
+
+#create sum area pH grassland and cropland
+JAC_module_23 <- JAC_module_23 %>% mutate(pH_sum = sum(get(area_ph_crop), get(area_ph_grass)))
+
+JAC_module_23 <- JAC_module_23 %>% mutate(merr5 = case_when(round(pH_sum, digits = 2) > round(get(total_area), digits = 2) ~ 1,
+                                                            TRUE ~0))
+
+JAC_module_23 <- JAC_module_23 %>% mutate(merr5_diff = case_when(merr5==1~abs(pH_sum-get(total_area)),
+                                                            TRUE ~0))
+
+
+#merr6 - sum of area of nutrient management plan (grassland and cropland) is greater than total area (item12)
+
+#create sum nutrient management grassland and cropland
+JAC_module_23 <- JAC_module_23 %>% mutate(nutrient_sum = sum(get(nutrient_plan_grass), get(nutrient_plan_crop)))
+
+JAC_module_23 <- JAC_module_23 %>% mutate(merr6 = case_when(round(nutrient_sum, digits = 2) > round(get(total_area), digits = 2) ~ 1,
+                                                            TRUE ~0))
+
+JAC_module_23 <- JAC_module_23 %>% mutate(merr6_diff = case_when(merr6==1~abs(nutrient_sum-get(total_area)),
+                                                                 TRUE ~0))
+
+#merr7- sum of total farmed area fertilised with mineral and manure fertilisers greater than total area (item12)
+
+#create sum for area fertilised with mineral and manure fertilisers 
+JAC_module_23 <- JAC_module_23 %>% mutate(fertiliser_sum = sum(get(fertiliser_mineral_area), get(fertiliser_organic_area)))
+
+JAC_module_23 <- JAC_module_23 %>% mutate(merr7 = case_when(round(fertiliser_sum, digits = 2) > round(get(total_area), digits = 2) ~ 1,
+                                                            TRUE ~0))
+
+JAC_module_23 <- JAC_module_23 %>% mutate(merr7_diff = case_when(merr7==1~abs(fertiliser_sum-get(total_area)),
+                                                                 TRUE ~0))
 
 
 
 #module error dataframes------------------------
 #merr1-----------------------------
 ##@knitr merr1
-merr1<- JAC_module_23%>% select(parish, holding, #submisType, 
-                                land_data, saf_data, all_of(c(broadcast_spreader_more_four_hours, broadcast_spreader_not_ploughed,
+merr1<- JAC_module_23%>% select(parish, holding, submisType, 
+                                land_data, saf_data, all_of(c(broadcast_spreader_less_four_hours, broadcast_spreader_more_four_hours, broadcast_spreader_not_ploughed,
                                                                                                        band_spreader_hose, band_spreader_shoe,open_slot_shallow_injection, 
                                                                                                        closed_slot_deep_injection, other_spread)), manure_slurry_spread_total, merr1)
 merr1 <-merr1%>%  mutate(error=ifelse(merr1== 1 | is.na(merr1), "Error", "No Error"))%>% filter(error == "Error")
 
 #merr2-----------------------------
 ##@knitr merr2
-merr2<- JAC_module_23%>% select(parish, holding, #submisType, 
+merr2<- JAC_module_23%>% select(parish, holding, submisType, 
                                 land_data, saf_data, all_of(c(storage_heaps, storage_compost,
                                                               storage_pits_below_animals, storage_deep_litter, storage_without_cover, 
                                                               storage_permeable_cover, storage_impermeable_cover, storage_other_facilities, 
@@ -272,9 +343,50 @@ merr2 <-merr2%>%  mutate(error=ifelse(merr2== 1 | is.na(merr2), "Error", "No Err
 
 #merr3-----------------------------
 ##@knitr merr3
-merr3<- JAC_module_23%>% select(parish, holding, #submisType, 
-                                land_data, saf_data, all_of(c(nutrient_plan_update, area_ph_crop, area_ph_grass)), merr3)
+merr3<- JAC_module_23%>% select(parish, holding, submisType, 
+                                land_data, saf_data, all_of(c(nutrient_plan_update, nutrient_plan_crop, nutrient_plan_grass)), merr3)
 merr3 <-merr3%>%  mutate(error=ifelse(merr3== 1 | is.na(merr3), "Error", "No Error"))%>% filter(error == "Error")
+
+
+#merr4-----------------------------
+##@knitr merr4
+merr4<- JAC_module_23%>% select(parish, holding, submisType, 
+                                land_data, saf_data, all_of(c(total_area, crop_rotation, not_crop_rotation, arable_mix_with_legume)), crop_rot_sum, merr4, merr4_diff)
+merr4 <-merr4%>%  mutate(error=ifelse(merr4== 1 | is.na(merr4), "Error", "No Error"))%>% filter(error == "Error")
+
+#check if people have portioned land between general crop rotation, not in general crop rotation and arable mix with legume
+merr4_check <- merr4 %>% filter(merr4, abs(crop_rot_sum - get(total_area)) == get(arable_mix_with_legume))
+
+#merr5-----------------------------
+##@knitr merr5
+merr5<- JAC_module_23%>% select(parish, holding, submisType, 
+                                land_data, saf_data, all_of(c(total_area, area_ph_crop, area_ph_grass)), pH_sum, merr5, merr5_diff)
+#check filter(merr5, pH_sum>get(total_area))
+merr5 <-merr5%>%  mutate(error=ifelse(merr5== 1 | is.na(merr5), "Error", "No Error"))%>% filter(error == "Error")
+
+#merr6-----------------------------
+##@knitr merr6
+merr6<- JAC_module_23%>% select(parish, holding, submisType, 
+                                land_data, saf_data, all_of(c(total_area, nutrient_plan_crop, nutrient_plan_grass)), nutrient_sum, merr6, merr6_diff)
+
+# check filter(merr6, nutrient_sum>get(total_area)) 
+merr6 <-merr6%>%  mutate(error=ifelse(merr6== 1 | is.na(merr6), "Error", "No Error"))%>% filter(error == "Error")
+
+#merr7-----------------------------
+##@knitr merr7
+merr7<- JAC_module_23%>% select(parish, holding, submisType, 
+                                land_data, saf_data, all_of(c(total_area, fertiliser_mineral_area, fertiliser_organic_area)), fertiliser_sum, merr7, merr7_diff)
+
+# check filter(merr7, fertiliser_sum>get(total_area)) 
+merr7 <-merr7%>%  mutate(error=ifelse(merr7== 1 | is.na(merr7), "Error", "No Error"))%>% filter(error == "Error")
+
+
+
+
+
+
+
+
 
 
 ##module error summary----------------------
@@ -282,5 +394,6 @@ merr_list <- c("merr1", "merr2", "merr3")
 module_errors <- JAC_module_23 %>% select(parish, holding, starts_with("merr"))%>% 
   filter(get(merr_list)!=0)
 
-
+JAC_module_23 %>% select(arable_mix_with_legume)
+  
                                              
