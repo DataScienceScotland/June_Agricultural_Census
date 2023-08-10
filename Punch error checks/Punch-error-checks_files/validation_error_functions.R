f9518dbb84a433ff62e54942c9259374a010227f
Massaya J (Jacqueline) <Jacqueline.Massaya@gov.scot>
2023-08-10 15:20:57 +0100

    deletes

--- Punch error checks/Punch-error-checks_files/validation_error_functions.R
+++
@@ -1,6 +0,0 @@
-
-## punch error checks function for selecting columns specific to error 
-keep_perr_vars <- function(x){
-  perr_x <<- errors %>% select(parish, holding, survtype, all_of(x))    
-  name_x <<- sub("\\_.*", "", substitute(x))
-}
